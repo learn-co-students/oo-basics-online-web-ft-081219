ba3764c1f634086008af0702ebf08b611953f894
Levi Borland <borland.levi@gmail.com>
2019-08-30 16:22:46 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
-# Make your shoe class here!
+class Shoe
+  
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  
+  def initialize(brand, color = "red", size = "9.5", material = "suede", condition = "tattered")
+    @brand = brand
+  end
+  
+  
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end 
